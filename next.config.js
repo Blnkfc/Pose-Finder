5215225016d52e0db15e94157b4998488739c2d5
Blnkfc <pavlinus11v@gmail.com>
2023-11-10 15:03:51 +0200

    Added missing files and fixed building issues hot fix.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 // next.config.js
-const path = require('path');
+
 
 module.exports = {
-    // ... other configurations
 
-    webpack: (config, { isServer }) => {
-        config.module.rules.push({
-            test: /\.scss$/,
-            use: [
-                'css-loader',
-                'sass-loader'
-            ],
-            include: path.resolve(__dirname, 'styles'),
-        });
-
-        return config;
-    },
 };

37932d709782ca49f2a2b25481420fc8b46ca07d
Blnkfc <pavlinus11v@gmail.com>
2023-11-15 02:28:54 +0200

    Hot Fix

--- app/FAQ/page.jsx
+++ app/FAQ/page.jsx
@@ -1,39 +1,44 @@
 'use client'
-import {useStore} from "@/src/store";
-import {useState} from "react";
+import { useStore } from "@/src/store";
+import { useState } from "react";
 
 const useExpandState = () => {
     const [expand, setExpand] = useState(false);
 
     const toggleExpand = () => {
-        setExpand(prevExpand => !prevExpand);
+        setExpand((prevExpand) => !prevExpand);
     };
 
     return [expand, toggleExpand];
 };
 
-const FAQ = () => {
-    const FAQData = useStore((state) => state.FAQList)
-
-    const FQAList = FAQData.map((E, index) => {
-        const [expand, toggleExpand] = useExpandState();
-        return(
-            <div className={"FAQ__item"} key={index} onClick={toggleExpand}>
-                <div
-                    className="FAQ__item__title"
-                    style={{backgroundColor:expand?"rgba(255,255,255,0.7)":"rgba(255,255,255,0.4)"}} >
-                    {E.title}
-                    <span style={{transform:expand?"rotate(90deg)":"rotate(-90deg)"}} > &#8249; </span>
-                </div>
-                <div className="FAQ__item__description" style={{display:expand?"flex":"none"}} >{E.description}</div>
-            </div>
-        )
-    })
+const FAQItem = ({ title, description }) => {
+    const [expand, toggleExpand] = useExpandState();
 
     return (
-        <div className={"FAQ"}>
-            {FQAList}
+        <div className={"FAQ__item"} onClick={toggleExpand}>
+            <div
+                className="FAQ__item__title"
+                style={{ backgroundColor: expand ? "rgba(255,255,255,0.7)" : "rgba(255,255,255,0.4)" }}
+            >
+                {title}
+                <span style={{ transform: expand ? "rotate(90deg)" : "rotate(-90deg)" }}> &#8249; </span>
+            </div>
+            <div className="FAQ__item__description" style={{ display: expand ? "flex" : "none" }}>
+                {description}
+            </div>
         </div>
-    )
-}
+    );
+};
+
+const FAQ = () => {
+    const FAQData = useStore((state) => state.FAQList);
+
+    const FQAList = FAQData.map((E, index) => (
+        <FAQItem key={index} title={E.title} description={E.description} />
+    ));
+
+    return <div className={"FAQ"}>{FQAList}</div>;
+};
+
 export default FAQ;
